a7ea535de68e93aeb1cc6c4a5c860c63eb871471
Brad Roberts <braddr@puremagic.com>
2017-08-31 21:56:15 -0700

    retire old naming scheme for bootstrap scripts

--- ec2-bootstrap/upload.sh
+++ ec2-bootstrap/upload.sh
@@ -3,10 +3,6 @@
 aws --profile at s3 cp scripts/at-client-bootstrap.sh          s3://client-bootstrap.auto-tester.puremagic.com/at-client-bootstrap.sh
 aws --profile at s3 cp scripts/ec2-user-at-client-bootstrap.sh s3://client-bootstrap.auto-tester.puremagic.com/ec2-user-at-client-bootstrap.sh
 
-# delete eventually
-aws --profile at s3 cp scripts/c3.large-bootstrap.sh           s3://client-bootstrap.auto-tester.puremagic.com/c3.large-bootstrap.sh
-aws --profile at s3 cp scripts/c4.large-bootstrap.sh           s3://client-bootstrap.auto-tester.puremagic.com/c4.large-bootstrap.sh
-
 # linux platforms
 aws --profile at s3 cp scripts/Linux-c3.large-bootstrap.sh     s3://client-bootstrap.auto-tester.puremagic.com/Linux-c3.large-bootstrap.sh
 aws --profile at s3 cp scripts/Linux-c4.large-bootstrap.sh     s3://client-bootstrap.auto-tester.puremagic.com/Linux-c4.large-bootstrap.sh
