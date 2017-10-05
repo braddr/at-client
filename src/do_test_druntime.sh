417c588e559118ecb6439eac273892199555873d
Sebastian Wilzbach <seb@wilzba.ch>
2017-10-06 01:49:42 +0200

    Remove hard-coded src/dmd executables

--- src/do_test_druntime.sh
+++ src/do_test_druntime.sh
@@ -16,7 +16,7 @@ if [ "$2" == "stub" ]; then
     exit 0
 fi
 
-$makecmd DMD=../dmd/src/dmd MODEL=$OUTPUT_MODEL $EXTRA_ARGS -f $makefile auto-tester-test >> ../druntime-unittest.log 2>&1
+$makecmd MODEL=$OUTPUT_MODEL $EXTRA_ARGS -f $makefile auto-tester-test >> ../druntime-unittest.log 2>&1
 if [ $? -ne 0 ]; then
     echo -e "\tdruntime unittest failed to build"
     exit 1
