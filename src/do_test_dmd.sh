7cbff407ccb5504b3e10270d506930687320b03b
Brad Roberts <braddr@puremagic.com>
2017-11-21 19:25:20 -0800

    add HOST_DC to the the env while testing dmd

--- src/do_test_dmd.sh
+++ src/do_test_dmd.sh
@@ -9,6 +9,21 @@
 
 . src/setup_env.sh "$2"
 
+# expose a prebuilt dmd
+if [ "${2:0:7}" == "Darwin_" -o "${2:0:6}" == "Win_32" ]; then
+    BINDIR=bin
+else
+    BINDIR=bin$COMPILER_MODEL
+fi
+HOST_DC=`ls -1 $PWD/release-build/install/*/$BINDIR/dmd$EXE`
+echo "HOST_DC=$HOST_DC" >> $1/dmd-unittest.log 2>&1
+
+if [[ ! -z "$HOST_DC" && ( "$2" == "Win_32" || "$2" == "Win_32_64" ) ]]; then
+    HOST_DC=`cygpath -w $HOST_DC`
+    echo "HOST_DC=$HOST_DC" >> $1/dmd-unittest.log 2>&1
+fi
+
+
 echo -e "\ttesting dmd"
 
 case "$2" in
