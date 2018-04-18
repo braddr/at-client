27db74d9c5eb902d1615483dc887443440858dfd
Brad Roberts <braddr@puremagic.com>
2018-04-17 20:34:16 -0700

    sigh, difference in distribution tarballs is annoying

--- src/do_cache_dmd.sh
+++ src/do_cache_dmd.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 set -x
 
@@ -9,36 +9,36 @@ fi
 
 DMDVER=$1
 OSTYPE=$2
-FILENAME=dmd.$DMDVER.$OSTYPE.tar.xz
-if [ "$OSTYPE" == "windows" ]; then
-    FILENAME=dmd.$DMDVER.$OSTYPE.zip
-fi
 
 if [ -d release-build/dmd-$DMDVER ]; then
     # cached dir already exists, assume correct and move on
     exit 0
+else
+    mkdir -p release-build/dmd-$DMDVER
 fi
 
 if [ ! -d zips ]; then
     mkdir zips
 fi
 
-if [ ! -f zips/$FILENAME ]; then
-    curl --silent -f -L -o zips/$FILENAME http://downloads.dlang.org/releases/2.x/$DMDVER/$FILENAME
-    if [ $? -ne 0 ]; then
-        echo "failed to download $FILENAME"
-        exit 1
-    fi
-fi
-
 if [ "$OSTYPE" == "windows" ]; then
+    FILENAME=dmd.$DMDVER.$OSTYPE.zip
+    curl --silent -f -L -o zips/$FILENAME http://downloads.dlang.org/releases/2.x/$DMDVER/$FILENAME
     unzip zips/$FILENAME -d release-build/dmd-$DMDVER
     mv release-build/dmd-$DMDVER/dmd2/windows release-build/dmd-$DMDVER/
     mv release-build/dmd-$DMDVER/dmd2/src     release-build/dmd-$DMDVER/
     rm -rf release-build/dmd-$DMDVER/dmd2
     chmod u+x release-build/dmd-$DMDVER/windows/bin*/*.{exe,dll}
+elif [ "$OSTYPE" == "freebsd" ]; then
+    FILENAME=dmd.$DMDVER.$OSTYPE-32.tar.xz
+    curl --silent -f -L -o zips/$FILENAME http://downloads.dlang.org/releases/2.x/$DMDVER/$FILENAME
+    tar -C release-build/dmd-$DMDVER --strip-components=1 -Jxf zips/$FILENAME dmd2/$OSTYPE dmd2/src
+
+    FILENAME=dmd.$DMDVER.$OSTYPE-64.tar.xz
+    curl --silent -f -L -o zips/$FILENAME http://downloads.dlang.org/releases/2.x/$DMDVER/$FILENAME
+    tar -C release-build/dmd-$DMDVER --strip-components=1 -Jxf zips/$FILENAME dmd2/$OSTYPE dmd2/src
 else
-    mkdir -p release-build/dmd-$DMDVER
+    FILENAME=dmd.$DMDVER.$OSTYPE.tar.xz
+    curl --silent -f -L -o zips/$FILENAME http://downloads.dlang.org/releases/2.x/$DMDVER/$FILENAME
     tar -C release-build/dmd-$DMDVER --strip-components=1 -Jxf zips/$FILENAME dmd2/$OSTYPE dmd2/src
 fi
-
